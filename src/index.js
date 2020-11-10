ebeaaa91f7e9877f81a15a4c0ab039b2ddd2d8ae
igaue-takahhiko <hasewo.2610@gmail.com>
2020-11-10 12:42:22 +0900

    implement getting started after webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-const component = () => {
+import _ from 'lodash';
+
+function component() {
     const element = document.createElement('div');
-    const array = ["hello", "webpack"]
-    element.innerHTML = _.join(array, " ")
+    const array = ["hello", "webpack"];
+    element.innerHTML = _.join(array, " ");
     return element;
 }
 
