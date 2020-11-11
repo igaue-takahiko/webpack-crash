4bf7286d48ab7f0d766c62ee349939c5f1cc00da
igaue-takahhiko <hasewo.2610@gmail.com>
2020-11-11 17:31:06 +0900

    implement url-loader and file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,14 @@ module.exports = {
                     'style-loader',
                     'css-loader',
                 ]
+            },
+            {
+                test: /\.(jpe?g|svg|ico|png|gif)$/i,
+                loader: 'url-loader',
+                options: {
+                    limit: 2048,
+                    name: './images/[name].[ext]'
+                }
             }
         ]
     },
