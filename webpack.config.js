8ea9abaee61663bfb35c838aa9d85c01ca23910a
igaue-takahhiko <hasewo.2610@gmail.com>
2020-11-12 12:14:02 +0900

    apply babel-preset-minify

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
                 use: {
                     loader: "babel-loader",
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: ["minify", '@babel/preset-env']
                     }
                 }
             },
